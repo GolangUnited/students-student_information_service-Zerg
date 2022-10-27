6d81af235b096b0ece4d00babe5b683e83630242
sa2dev <99678218+sa2dev@users.noreply.github.com>
2022-10-27 13:36:16 +0400

    Create migrations (#9)
    * Migration init schema
    
    * Fix makefile and migration
    
    * Delete comment
    
    * Make commands to create and drop db
    
    * Add install migrate tool and set db parameters
    
    * Added lost changes
    
    * Fix db parameter

--- Makefile
+++ Makefile
@@ -13,6 +13,25 @@ GO_BIN_PATH = $(shell go env GOPATH)/bin
 # Full path to linter
 GOLANGCI_LINT = $(GO_BIN_PATH)/golangci-lint
 
+# Full path to migrate tool
+MIGRATE_TOOL = $(GO_BIN_PATH)/migrate
+
+# DB parameters
+DB_HOST = localhost
+DB_PORT = 5432
+DB_NAME = student_information_srv
+DB_USER_NAME = root
+DB_USER_PASSWORD = secret
+
+# DB connection string
+CONNECTION_STRING = postgresql://$(DB_USER_NAME):$(DB_USER_PASSWORD)@$(DB_HOST):$(DB_PORT)
+
+# Path to migration files
+MIGRATION_PATH = internal/storage/db/migration
+
+# Migration tool command string
+MIGRATION_STRING = $(MIGRATE_TOOL)/migrate -path $(MIGRATION_PATH) -database "$(CONNECTION_STRING)/$(DB_NAME)?sslmode=disable" -verbose
+
 .SILENT:
 
 build:
@@ -25,6 +44,20 @@ run: build
 test:
 	go test -v ./...
 
+createdb:
+	psql $(CONNECTION_STRING) -c 'CREATE DATABASE ${DB_NAME};'
+
+dropdb:
+	psql $(CONNECTION_STRING) -c 'DROP DATABASE ${DB_NAME};'
+
+.PHONY: migrateup
+migrateup: .install-migrate-tool
+	$(MIGRATION_STRING) up
+
+.PHONY: migratedown
+migratedown: .install-migrate-tool
+	$(MIGRATION_STRING) down
+
 .PHONY: .install-linter
 .install-linter:
 	[ -f $(GOLANGCI_LINT) ] || curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(GO_BIN_PATH) v1.50.0
@@ -36,3 +69,7 @@ lint: .install-linter
 .PHONY: lint-fast
 lint-fast: .install-linter
 	$(GOLANGCI_LINT) run ./... --fast
+
+.PHONY: .install-migrate-tool
+.install-migrate-tool:
+	GOBIN=$(MIGRATE_TOOL) go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@v4.15.2
---
+++ internal/storage/db/migration/000001_init_schema.up.sql
@@ -0,0 +1,122 @@
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "first_name" varchar NOT NULL,
+  "last_name" varchar NOT NULL,
+  "birthday" timestamp NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "password_hash" varchar NOT NULL
+);
+
+CREATE TABLE "students" (
+  "id" int PRIMARY KEY,
+  "group_id" int NOT NULL
+);
+
+CREATE TABLE "mentors" (
+  "id" int PRIMARY KEY
+);
+
+CREATE TABLE "admins" (
+  "id" int
+);
+
+CREATE TABLE "student_details" (
+  "student_id" int,
+  "resume" text,
+  "mentor_review" text,
+  "reviewer_id" int
+);
+
+CREATE TABLE "skills" (
+  "skill" varchar PRIMARY KEY
+);
+
+CREATE TABLE "student_skills" (
+  "student_id" int NOT NULL,
+  "skill" varchar NOT NULL,
+  "level" int NOT NULL
+);
+
+CREATE TABLE "contacts" (
+  "student_id" int,
+  "discord" varchar,
+  "telegram" varchar,
+  "instagram" varchar
+);
+
+CREATE TABLE "groups" (
+  "id" int UNIQUE PRIMARY KEY,
+  "mentor" int NOT NULL,
+  "date_start" timestamp NOT NULL,
+  "date_end" timestamp NOT NULL
+);
+
+CREATE TABLE "group_overview" (
+  "group_id" int,
+  "overview" text NOT NULL,
+  "reviewer_id" int NOT NULL
+);
+
+CREATE TABLE "homework" (
+  "student_id" int,
+  "hommework_score_1" int,
+  "hommework_score_2" int,
+  "hommework_score_3" int,
+  "hommework_score_4" int,
+  "hommework_score_5" int,
+  "hommework_score_6" int
+);
+
+CREATE TABLE "diploma" (
+  "student_id" int NOT NULL,
+  "theme" varchar NOT NULL,
+  "description" text
+);
+
+CREATE TABLE "cert" (
+  "student_id" int NOT NULL,
+  "cert_id" varchar UNIQUE NOT NULL,
+  "issued" timestamp NOT NULL
+);
+
+CREATE TABLE "interview" (
+  "student_id" int,
+  "mentor_id" int,
+  "date" timestamp NOT NULL,
+  "mark" int NOT NULL,
+  "notes" text
+);
+
+ALTER TABLE "students" ADD FOREIGN KEY ("id") REFERENCES "users" ("id");
+
+ALTER TABLE "students" ADD FOREIGN KEY ("group_id") REFERENCES "groups" ("id");
+
+ALTER TABLE "mentors" ADD FOREIGN KEY ("id") REFERENCES "users" ("id");
+
+ALTER TABLE "admins" ADD FOREIGN KEY ("id") REFERENCES "users" ("id");
+
+ALTER TABLE "student_details" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
+
+ALTER TABLE "student_details" ADD FOREIGN KEY ("reviewer_id") REFERENCES "mentors" ("id");
+
+ALTER TABLE "student_skills" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
+
+ALTER TABLE "student_skills" ADD FOREIGN KEY ("skill") REFERENCES "skills" ("skill");
+
+ALTER TABLE "contacts" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
+
+ALTER TABLE "groups" ADD FOREIGN KEY ("mentor") REFERENCES "mentors" ("id");
+
+ALTER TABLE "group_overview" ADD FOREIGN KEY ("group_id") REFERENCES "groups" ("id");
+
+ALTER TABLE "group_overview" ADD FOREIGN KEY ("reviewer_id") REFERENCES "mentors" ("id");
+
+ALTER TABLE "homework" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
+
+ALTER TABLE "diploma" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
+
+ALTER TABLE "cert" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
+
+ALTER TABLE "interview" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
+
+ALTER TABLE "interview" ADD FOREIGN KEY ("mentor_id") REFERENCES "mentors" ("id");
