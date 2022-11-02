9e274ca6a49d74ce646043eb47c1c50fd3711998
Владимир Артёмов <iorganium@gmail.com>
2022-11-02 23:17:05 +0500

    func for token parsing

--- internal/jwt/jwt.go
+++ internal/jwt/jwt.go
@@ -1,16 +1,37 @@
 package jwt
 
 import (
+	"fmt"
 	"github.com/golang-jwt/jwt/v4"
+	"os"
 	"zerg-team-student-information-service/internal/models"
 )
 
-var secret = "secret_string"
+const JwtEnvKey = "JWT_SECRET"
 
 func GetUserToken(user models.User) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email": user.Email,
 	})
 
-	return token.SignedString([]byte(secret))
+	return token.SignedString([]byte(os.Getenv(JwtEnvKey)))
+}
+
+func GetDataFromToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv(JwtEnvKey)), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("token setup error")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, fmt.Errorf("token parsing error")
+	}
+
+	return claims, nil
 }
--- internal/jwt/jwt_test.go
+++ internal/jwt/jwt_test.go
@@ -2,17 +2,33 @@ package jwt_test
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 	"zerg-team-student-information-service/internal/jwt"
 	"zerg-team-student-information-service/internal/models"
 )
 
-func TestGetUserToken(t *testing.T) {
-	user := models.User{
-		Email: "test@example.com",
-	}
+const testTokenString = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZXhhbXBsZS5jb20ifQ.1Z5vO0nl_5A7llG_c6wgKeZvmBtGh2LMDmr5CceSdgo"
+const testSecret = "test_secret_string"
 
-	token, err := jwt.GetUserToken(user)
+var testUser = models.User{Email: "test@example.com"}
+
+func TestGetUserToken(t *testing.T) {
+	os.Setenv(jwt.JwtEnvKey, testSecret)
+	defer os.Unsetenv(jwt.JwtEnvKey)
+
+	want := testTokenString
+	got, err := jwt.GetUserToken(testUser)
 	assert.NoError(t, err)
-	assert.Greater(t, len(token), 10)
+	assert.Equal(t, want, got)
+}
+
+func TestGetDataFromToken(t *testing.T) {
+	os.Setenv(jwt.JwtEnvKey, testSecret)
+	defer os.Unsetenv(jwt.JwtEnvKey)
+
+	data, err := jwt.GetDataFromToken(testTokenString)
+	assert.NoError(t, err)
+	assert.NotNil(t, data)
+	assert.Equal(t, testUser.Email, data["email"])
 }
