f874bfffb82952aebf28c7cdd2a4e5a552f06e90
Владимир Артёмов <iorganium@gmail.com>
2022-11-05 01:18:42 +0500

    refactoring

--- internal/jwt/jwt.go
+++ internal/jwt/jwt.go
@@ -4,34 +4,40 @@ import (
 	"fmt"
 	"github.com/golang-jwt/jwt/v4"
 	"os"
+	"time"
 	"zerg-team-student-information-service/internal/models"
 )
 
 const JwtEnvKey = "JWT_SECRET"
 
-func GetUserToken(user models.User) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"email": user.Email,
+type TokenData struct {
+	Email string
+	jwt.RegisteredClaims
+}
+
+func GenerateUserToken(user models.User) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &TokenData{
+		Email: user.Email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
 	})
 
 	return token.SignedString([]byte(os.Getenv(JwtEnvKey)))
 }
 
-func GetDataFromToken(tokenString string) (jwt.MapClaims, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
+func GetDataFromToken(tokenString string) (*TokenData, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &TokenData{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv(JwtEnvKey)), nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("token setup error")
+		return nil, fmt.Errorf("token parsing error: %w", err)
 	}
 
-	claims, ok := token.Claims.(jwt.MapClaims)
+	data, ok := token.Claims.(*TokenData)
 	if !ok || !token.Valid {
 		return nil, fmt.Errorf("token parsing error")
 	}
 
-	return claims, nil
+	return data, nil
 }
--- internal/jwt/jwt_test.go
+++ internal/jwt/jwt_test.go
@@ -8,27 +8,19 @@ import (
 	"zerg-team-student-information-service/internal/models"
 )
 
-const testTokenString = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAZXhhbXBsZS5jb20ifQ.1Z5vO0nl_5A7llG_c6wgKeZvmBtGh2LMDmr5CceSdgo"
 const testSecret = "test_secret_string"
 
 var testUser = models.User{Email: "test@example.com"}
 
-func TestGetUserToken(t *testing.T) {
+func TestUserToken(t *testing.T) {
 	os.Setenv(jwt.JwtEnvKey, testSecret)
 	defer os.Unsetenv(jwt.JwtEnvKey)
 
-	want := testTokenString
-	got, err := jwt.GetUserToken(testUser)
+	tokenString, err := jwt.GenerateUserToken(testUser)
 	assert.NoError(t, err)
-	assert.Equal(t, want, got)
-}
+	assert.Greater(t, len(tokenString), 5)
 
-func TestGetDataFromToken(t *testing.T) {
-	os.Setenv(jwt.JwtEnvKey, testSecret)
-	defer os.Unsetenv(jwt.JwtEnvKey)
-
-	data, err := jwt.GetDataFromToken(testTokenString)
+	data, err := jwt.GetDataFromToken(tokenString)
 	assert.NoError(t, err)
-	assert.NotNil(t, data)
-	assert.Equal(t, testUser.Email, data["email"])
+	assert.Equal(t, testUser.Email, data.Email)
 }
