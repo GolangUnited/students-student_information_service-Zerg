9b6b316c9ba9dda407c2ba26bb7a281ff4ed3cd6
Max Reshetnikov <maxim.a.reshetnikov@gmail.com>
2022-11-22 10:09:07 +0400

    Added tests TestGetByID and TestGetByEmail

--- internal/storage/postgress/user_test.go
+++ internal/storage/postgress/user_test.go
@@ -8,18 +8,56 @@ import (
 	"zerg-team-student-information-service/internal/storage/postgress"
 )
 
+var userModel = models.User{ID: 7, FirstName: "Ivan", LastName: "Sidorov", Birthday: "30.01.1985",
+	Email: "ivan.sidorov@mail.ru", PasswordHash: "sst67a8dfge638"}
+
+var columns = []string{"id", "first_name", "last_name", "birthday", "email", "password_hash"}
+
 func TestCreateUser(t *testing.T) {
 	dbConnect, _ := postgress.NewMockConnect()
 	defer dbConnect.Close()
 
-	user := models.User{FirstName: "Ivan", LastName: "Sidorov", Birthday: "30.01.1985",
-		Email: "ivan.sidorov@mail.ru", PasswordHash: "sst67a8dfge638"}
-	dbConnect.Mock.ExpectExec("INSERT INTO users").WithArgs(user.FirstName, user.LastName,
-		user.Birthday, user.Email, user.PasswordHash).WillReturnResult(sqlmock.NewResult(5, 1))
+	userId := int64(5)
+	dbConnect.Mock.ExpectExec("INSERT INTO users").
+		WithArgs(userModel.FirstName, userModel.LastName,
+			userModel.Birthday, userModel.Email, userModel.PasswordHash).
+		WillReturnResult(sqlmock.NewResult(userId, 1))
 
 	db := postgress.NewUserDB(dbConnect)
-	userId, err := db.Create(user)
+	id, err := db.Create(userModel)
 
 	assert.Equal(t, nil, err)
-	assert.Equal(t, int64(5), userId)
+	assert.Equal(t, userId, id)
+}
+
+func TestGetByEmail(t *testing.T) {
+	dbConnect, _ := postgress.NewMockConnect()
+	defer dbConnect.Close()
+
+	dbConnect.Mock.ExpectQuery(`SELECT \* FROM users WHERE email=(.+)`).
+		WithArgs(userModel.Email).
+		WillReturnRows(sqlmock.NewRows(columns).AddRow(userModel.ID, userModel.FirstName, userModel.LastName,
+			userModel.Birthday, userModel.Email, userModel.PasswordHash))
+
+	db := postgress.NewUserDB(dbConnect)
+	user, err := db.GetByEmail(userModel.Email)
+
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, userModel, user)
+}
+
+func TestGetByID(t *testing.T) {
+	dbConnect, _ := postgress.NewMockConnect()
+	defer dbConnect.Close()
+
+	dbConnect.Mock.ExpectQuery(`SELECT \* FROM users WHERE id=(.+)`).
+		WithArgs(userModel.ID).
+		WillReturnRows(sqlmock.NewRows(columns).AddRow(userModel.ID, userModel.FirstName, userModel.LastName,
+			userModel.Birthday, userModel.Email, userModel.PasswordHash))
+
+	db := postgress.NewUserDB(dbConnect)
+	user, err := db.GetByID(userModel.ID)
+
+	assert.Equal(t, nil, err)
+	assert.EqualValues(t, userModel, user)
 }
